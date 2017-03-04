e1ea625ef5ccaca1e2a2565363b19ec58c75104d
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-04 10:46:06 +0100

    Add default route

--- src/monitor/handler.clj
+++ src/monitor/handler.clj
@@ -1,21 +1,22 @@
 (ns monitor.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.middleware.json :refer [wrap-json-params]])
+            [ring.middleware.json :refer [wrap-json-params wrap-json-response]]
+            [ring.util.response :refer [response]]
+            [cognitect.transit :as transit])
   (:use [clojure.repl :refer [source]]))
 
 (defroutes app-routes
-  #_(GET "/" [] {:status "ok"
-               :data "Back when PHP had less than 100 functions and the function hashing mechanism was strlen()"})
+  (GET "/" [] (response {:status :ok
+                         :data {:quote "Back when PHP had less than 100 functions and the function hashing mechanism was strlen()"
+                                :author "The author of PHP"}}))
   (POST "/" request
         request)
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
-      #_(wrap-defaults api-defaults)
       wrap-keyword-params
-      wrap-json-params))
+      wrap-json-params
+      (wrap-json-response app-routes)))
